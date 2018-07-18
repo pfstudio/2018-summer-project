55bda4d823497622cc466c07c18673cd618beeed
yiluomyt <mytyiluo@outlook.com>
2018-07-18 12:53:51 +0800

    修改Student控制器

--- src/iwebshop/controllers/student.php
+++ src/iwebshop/controllers/student.php
@@ -2,48 +2,56 @@
 /**
  * @brief 学生模块
  * @class Student
+ * @author yiluomyt
  */
 class Student extends IController
 {
     /**
      * @brief 获取单条学生信息
+     * @param id
      */
     function get()
     {
-        $user_id = IFilter::act(IReq::get('id'));
-        echo $user_id;
+
     }
 
     /**
      * @brief 新建学生
+     * @param phone, pin
      */
     function create()
     {
-        // 获取用户名和密码
-        $username = IFilter::act(IReq::get('username'));
-        $password = IReq::get('password');
-        // 检验用户名和密码格式
-        if(!$username || $password == '')
+        // 获取手机号和验证码
+        // IFilter::act 过滤传入的参数并转换为对应类型，默认为string
+        // IReq::get 获取参数
+        $phone = IFilter::act(IReq::get('phone'));
+        $pin   = IFilter::act(IReq::get('pin'), 'int');
+        // 检验手机号和验证码
+        // 验证码模块还未完成，默认通过
+        if(!($phone && true))
         {
-            die(JsonResult::fail('请提供正确的用户名和密码'));
+            die(JsonResult::fail('验证码错误'));
         }
         // 创建数据库对象
         $userDB = new IModel('user');
         $studentDB = new IModel('student');
-        // 检验用户名状态
-        if($userDB->getObj("username='".$username."'"))
+        // 检验手机号是否重复
+        if($userDB->getObj("phone='".$phone."'"))
         {
-            die(JsonResult::fail('用户名重复'));
+            die(JsonResult::fail('该手机号已被注册'));
         }
         // 创建用户对象
         $user = array(
-            'username' => $username,
-            'password' => md5($password)
+            'phone' => $phone,
+            'role'  => 0,
+            'create_time' => date('Y-m-d H:i:s')
         );
         // 添加用户记录
         $userDB->setData($user);
+        // 添加返回的是user的id
         $user_id = $userDB->add();
         // 初始化学生信息
+        // 这里只初始化user_id
         $student = array('user_id' => $user_id);
         $studentDB->setData($student);
         $studentDB->add();
@@ -53,49 +61,35 @@ class Student extends IController
 
     /**
      * @brief 更新学生信息
+     * @param 对应student表中的字段, id必填
      */
     function update()
     {
         // 获取参数
         $user_id = IFilter::act(IReq::get('id'), 'int');
-        $name = IFilter::act(IReq::get('name'));
-        $phone = IFilter::act(IReq::get('phone'));
-        $parents_phone = IFilter::act(IReq::get('parents_phone'));
-        $area = IFilter::act(IReq::get('area'));
-        $address = IFilter::act(IReq::get('address'));
-        $qq = IFilter::act(IReq::get('qq'));
-        $wechat = IFilter::act(IReq::get('wechat'));
+        // 其他参数
         $sex = IFilter::act(IReq::get('sex'), 'int');
-        $birthday = IFilter::act(IReq::get('birthday'));
-        $email = IFilter::act(IReq::get('email'));
-        $grade = IFilter::act(IReq::get('grade'));
+        // ...
 
         // 创建数据库对象
         $userDB = new IModel('user');
         $studentDB = new IModel('student');
 
-        // 检验参数格式
-        if(!$user_id || !$userDB->getObj('id = '.$user_id)
-        || !$studentDB->getObj('user_id = '.$user_id.' and status = 0'))
+        // 检验用户是否存在
+        // is_del是软删除字段
+        if(!$user_id || !$userDB->getObj('id = '.$user_id.' and is_del = 0'))
         {
             die(JsonResult::fail('该用户不存在'));
         }
 
-        if(!($sex == 0 || $sex == 1))
+        // 检验其他参数
+        if($sex != 0 && $sex != 1)
         {
-            $this->setError('性别仅限 0:男 1:女');
+            $this->setError('性别只能为 0: 男 1: 女');
         }
+        // ...
 
-		if($email && $memberDB->getObj("email='".$email."' and user_id != ".$user_id))
-		{
-			$this->setError('邮箱重复');
-		}
-
-		if($phone && $memberDB->getObj("phone='".$phone."' and user_id != ".$user_id))
-		{
-			$this->setError('手机号码重复');
-		}
-
+        // 若参数检验过程中有错误，返回错误信息
         if($errors = $this->getAllError())
         {
             die(JsonResult::fail($errors));
@@ -103,17 +97,7 @@ class Student extends IController
 
         // 创建对象
         $student = array(
-            'name' => $name,
-            'phone' => $phone,
-            'parents_phone' => $parents_phone,
-            'area' => $area,
-            'address' => $address,
-            'qq' => $qq,
-            'wechat' => $wechat,
-            'sex' => $sex,
-            'birthday' => $birthday,
-            'email' => $email,
-            'grade' => $grade
+            'sex' => $sex
         );
         $studentDB->setData($student);
         $studentDB->update('user_id = '.$user_id);
@@ -123,18 +107,19 @@ class Student extends IController
 
     /**
      * 删除学生
+     * @param id, true_del
      */
     function delete()
     {
-        $user_id = IFilter::act(IReq::get('id'), 'int');
+        $id = IFilter::act(IReq::get('id'), 'int');
         $true_del = IFilter::act(IReq::get('true_del'), 'bool');
 
         // 创建数据库对象
         $userDB = new IModel('user');
-        $studentDB = new IModel('student');
-        
+
+        // TODO: 这里把判断逻辑改一下分别对应软/硬删除
         // 检验参数状态
-        if(!$user_id || !$userDB->getObj('id = '.$user_id))
+        if(!$id || !$userDB->getObj('id = '.$id.' and is_del = 0'))
         {
             die(JsonResult::fail('该用户不存在'));
         }
@@ -142,15 +127,16 @@ class Student extends IController
         if($true_del)
         // 硬删除
         {
-            $userDB->del('id = '.$user_id);
+            $userDB->del('id = '.$id);
         }
         else
         // 软删除
         {
-            $studentDB->setData(array(
-                'status' => 1
+            // 软删除将is_del设为1
+            $userDB->setData(array(
+                'is_del' => 1
             ));
-            $studentDB->update('user_id = '.$user_id);
+            $userDB->update('id = '.$id);
         }
 
         echo JsonResult::success();
