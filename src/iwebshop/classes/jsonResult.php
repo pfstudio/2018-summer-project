25a344fffe2b75837aa31e1d73e3883e181b1a59
yiluomyt <mytyiluo@outlook.com>
2018-07-22 13:37:22 +0800

    添加JSON的Headers信息

--- src/iwebshop/classes/jsonResult.php
+++ src/iwebshop/classes/jsonResult.php
@@ -1,8 +1,17 @@
 <?php
+/**
+ * 规范化返回的JSON结果
+ */
 class JsonResult
 {
+    /**
+     * 操作成功
+     * @param $data 需要返回的数据，默认为null
+     */
     public static function success($data = null)
     {
+        // 设置返回内容为json
+        header('Content-type:application/json');
         return JSON::encode(array(
             'data'   => $data,
             'status' => 'success',
@@ -10,8 +19,14 @@ class JsonResult
         ));
     }
 
+    /**
+     * 操作失败
+     * @param $error 错误信息
+     */
     public static function fail($error)
     {
+        // 设置返回内容为json
+        header('Content-type:application/json');
         // 将返回的错误转换为统一的数组形式
         if(!is_array($error))
         {
